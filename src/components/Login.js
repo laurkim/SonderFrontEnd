17dd5fd14642078f3ca3e4756e18d03552f44313
laurkim <laurakeem@gmail.com>
2018-01-03 15:58:41 -0500

    style login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,64 @@
 import React from 'react';
-import { Image, Button, Container, Header, Icon } from 'semantic-ui-react';
+import { Image, Button, Container, Header, Icon, Segment } from 'semantic-ui-react';
 
 const Login = () => {
   return (
     <div>
-      {/* <Container textAlign='center'> */}
-        {/* <br/>
-        <Header size='huge'>Sonder</Header> */}
-        <Image src='https://media.giphy.com/media/wR3yklvpBIgzS/giphy.gif' fluid  />
-        <div id="login-button">
-          <Button animated basic inverted color="white" size="big" href='http://localhost:3000/api/v1/login'>
-          <Button.Content visible>Log In</Button.Content>
-          <Button.Content hidden>
-            <Icon name='send' />
-          </Button.Content>
-        </Button>
-      </div>
-    {/* </Container> */}
-  </div>
-);
+      <Segment
+        textAlign="center"
+        vertical
+        style={{
+          minHeight: 900,
+          backgroundImage: "url(https://media.giphy.com/media/wR3yklvpBIgzS/giphy.gif)",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+        }}
+        >
+          <Container text>
+            <Header
+              as="h1"
+              content="SONDER"
+              inverted
+              style={{
+                fontFamily: "Josefin Sans",
+                fontSize: "75px",
+                fontWeight: "normal",
+                marginBottom: 0,
+                marginTop: "8px"
+              }}
+            />
+        </Container>
+        <Container >
+          <Button
+            animated
+            basic
+            inverted
+            color="white"
+            size="large"
+            href='http://localhost:3000/api/v1/login'
+            style={{
+              marginTop: "575px"
+            }}>
+            <Button.Content
+              visible
+              content="LOG IN"
+              style={{
+                fontFamily: "Josefin Sans",
+                fontWeight: "normal",
+                marginTop: "4px"
+              }}
+            />
+            <Button.Content hidden>
+              <Icon name='send' />
+            </Button.Content>
+          </Button>
+        </Container>
+      </Segment>
+      {/* <div id="login-button"> */}
+    {/* </div> */}
+    </div>
+  );
+
 };
 
 export default Login;
