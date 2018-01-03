a3e25dad5dc20eaedfe4e8c1721ef1ea7e2c3b11
laurkim <laurakeem@gmail.com>
2018-01-03 09:53:14 -0500

    add custom styling for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,19 +4,19 @@ import { Image, Button, Container, Header, Icon } from 'semantic-ui-react';
 const Login = () => {
   return (
     <div>
-      <Container textAlign='center'>
-        <br/>
-        <Header size='huge'>Sonder</Header>
-        <Image src='https://media.giphy.com/media/wR3yklvpBIgzS/giphy.gif' fluid />
+      {/* <Container textAlign='center'> */}
+        {/* <br/>
+        <Header size='huge'>Sonder</Header> */}
+        <Image src='https://media.giphy.com/media/wR3yklvpBIgzS/giphy.gif' fluid  />
         <div id="login-button">
-          <Button animated basic inverted color="green" size="big" href='http://localhost:3000/api/v1/login'>
+          <Button animated basic inverted color="white" size="big" href='http://localhost:3000/api/v1/login'>
           <Button.Content visible>Log In</Button.Content>
           <Button.Content hidden>
-            <Icon name='right arrow' />
+            <Icon name='send' />
           </Button.Content>
         </Button>
       </div>
-    </Container>
+    {/* </Container> */}
   </div>
 );
 };
